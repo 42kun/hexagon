64c1934216abdb16355c8a6c02a8e3921bbce7b0
42kun <759382530@qq.com>
2019-09-12 09:22:50 +0800

    Hex5(2) 临时存档

--- Assets/Scripts/Camera/HexMapCamera.cs
+++ Assets/Scripts/Camera/HexMapCamera.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class HexMapCamera : MonoBehaviour
 {
+    /// <summary>
+    /// swivel 调整角度
+    /// stick 调整视距
+    /// </summary>
     Transform swivel, stick;
+
+    public float swivelMinZoom = 90, swivelMaxZoom = 45;
+
+    /// <summary>
+    /// 记录视距，0为最远，1为最近
+    /// </summary>
+    float zoom = 1f;
+    //最远距离，最近距离
+    public float stickMinZoom = -250, StickMaxZoom = -45;
 
     private void Awake()
     {
@@ -20,6 +33,24 @@ public class HexMapCamera : MonoBehaviour
 
     void Update()
     {
-        
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if(Mathf.Abs(zoomDelta) >= 0.01f)
+        {
+            AdjustZoom(zoomDelta);
+        }
+    }
+
+    /// <summary>
+    /// 接收增量，调整视距，然后把它限制在0-1之间
+    /// </summary>
+    /// <param name="delta"></param>
+    void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+        float distance = Mathf.Lerp(stickMinZoom, StickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
     }
 }
